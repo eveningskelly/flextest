9ebf8713d69e894bf45008c9ea820f533d65b076
Evening Skelly <eveningskelly@gmail.com>
2025-07-16 20:04:27 -0700

    Apply latest edits to Streamlit app

--- flextest.py
+++ flextest.py
@@ -53,7 +53,7 @@ st.markdown("""
 
 col_logo1, col_logo2, col_logo3 = st.columns([1, 6, 1])
 with col_logo1:
-    st.image("flexlogo.png", width=180)
+    st.image("flexlogo.png", width=200)
 with col_logo2:
     st.markdown('<h1 class="custom-title">Flex Analysis Report</h1>', unsafe_allow_html=True)
 with col_logo3:
